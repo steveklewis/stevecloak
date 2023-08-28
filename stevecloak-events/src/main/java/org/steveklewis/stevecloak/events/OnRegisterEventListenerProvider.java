6dada44e34ece5889a8ad4bfbc580a7fc8af0126
Steve Lewis <lewisste@argodata.com>
2023-08-28 03:36:56 -0500

    Logging user if a registration event, otherwise log the type.

--- stevecloak-events/src/main/java/org/steveklewis/stevecloak/events/OnRegisterEventListenerProvider.java
+++ stevecloak-events/src/main/java/org/steveklewis/stevecloak/events/OnRegisterEventListenerProvider.java
@@ -1,11 +1,18 @@
 package org.steveklewis.stevecloak.events;
 
+import org.jboss.logging.Logger;
+
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 
 public class OnRegisterEventListenerProvider implements EventListenerProvider {
+
+    private static final Logger log = Logger.getLogger(OnRegisterEventListenerProvider.class);
 
     KeycloakSession session;
     public OnRegisterEventListenerProvider(KeycloakSession session) {
@@ -14,7 +21,14 @@ public class OnRegisterEventListenerProvider implements EventListenerProvider {
 
     @Override
     public void onEvent(Event event) {
-
+        if (event.getType().equals(EventType.REGISTER)) {
+            log.info("RegisterEvent!");
+            RealmModel realm = session.realms().getRealm(event.getRealmId());
+            UserModel user = session.users().getUserById(realm, event.getUserId());
+            log.info("User found: " + user.getUsername());
+        } else {
+            log.info("onEvent: " + event.getType().name());
+        }
     }
 
     @Override
